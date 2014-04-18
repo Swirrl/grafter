36a4ed0e1ec25ec2d9251ad7890095535522d68f
Rick Moynihan <rick.m@swirrl.com>
2014-04-18 12:17:11 +0100

    Simple shapefile browser thanks to geotools.

--- project.clj
+++ project.clj
@@ -3,11 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :repositories [["geotools" "http://download.osgeo.org/webdav/geotools/"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  ;;[org.apache.jena/jena-core "2.11.1"]
                  ;;[plaza "0.0.5-SNAPSHOT"]
                  [org.clojure/math.combinatorics "0.0.7"]
                  [clojure-csv/clojure-csv "2.0.1"]
                  [org.openrdf.sesame/sesame-runtime "2.7.10"]
+                 [org.geotools/gt-main "2.7-M3"]
+                 [org.geotools/gt-swing "2.7-M3"]
+                 [org.geotools/gt-shapefile "2.7-M3"]
                  [seesaw "1.4.4"]]
   :java-source-path "src")
