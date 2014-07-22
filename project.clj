d205f27b3c251a96768a90c46fc761ed69d24556
Rick Moynihan <rick.m@swirrl.com>
2014-07-22 17:57:20 +0100

    Forgot to commit dependencies for last commit (remote sparql/http endpoint support)
    Conflicts:
    	project.clj

--- project.clj
+++ project.clj
@@ -14,7 +14,16 @@
                  [org.clojure/tools.namespace "0.2.4"]
                  [clojure-csv/clojure-csv "2.0.1"]
                  [org.marianoguerra/clj-rhino "0.2.1"]
-                 [org.openrdf.sesame/sesame-runtime "2.7.10"]]
+                 [org.openrdf.sesame/sesame-runtime "2.7.12"]
+                 [commons-httpclient/commons-httpclient "3.1"] ;; FIXME: bodge to fix strange transitive dependency issue - sesame should bring this in for us
+                 [potemkin "0.3.4"]
+                 [incanter "1.5.5"] ; Include all of incanter
+                 ]
+
+  ;;:dev-dependencies [[lein-autodoc "0.9.0"]]
+
+  :codox {:defaults {:doc "FIXME: write docs"}}
+
   :source-paths ["src"]
 
   :plugins [[com.aphyr/prism "0.1.1"] ;; autotest style support simply run: lein prism
