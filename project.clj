1415a2ecf9eebd72941e672e078e1b91759821db
Rick Moynihan <rick.m@swirrl.com>
2014-07-22 17:57:20 +0100

    Forgot to commit dependencies for last commit (remote sparql/http endpoint support)

--- project.clj
+++ project.clj
@@ -9,7 +9,6 @@
                                     :passphrase :env}]]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 ;;[org.apache.jena/jena-core "2.11.1"]
                  [org.clojure/algo.monads "0.1.5"]
                  [clj-time "0.7.0"]
                  [org.clojure/tools.namespace "0.2.4"]
@@ -17,9 +16,9 @@
                  [clj-excel "0.0.1"]
                  [me.raynes/fs "1.4.4"]
                  [org.marianoguerra/clj-rhino "0.2.1"]
-                 [org.openrdf.sesame/sesame-runtime "2.7.10"]
+                 [org.openrdf.sesame/sesame-runtime "2.7.12"]
+                 [commons-httpclient/commons-httpclient "3.1"] ;; FIXME: bodge to fix strange transitive dependency issue - sesame should bring this in for us
                  [potemkin "0.3.4"]
-                 ;;[incanter/incanter-core "1.5.5"]
                  [incanter "1.5.5"] ; Include all of incanter
                  ]
   ;;:dev-dependencies [[lein-autodoc "0.9.0"]]
