4e00c714162af9551c1b64bed809550c5ac9cff9
Robin Gower <robin.git@infonomics.ltd.uk>
2014-07-30 14:02:31 +0200

    Update vocabularies: pmd and foaf

--- src/grafter/rdf/ontologies/pmd.clj
+++ src/grafter/rdf/ontologies/pmd.clj
@@ -1,10 +1,19 @@
 (ns grafter.rdf.ontologies.pmd
   (:use [grafter.rdf.ontologies.util]))
 
-(def pmd (prefixer "http://publishmydata.com/def/dataset#"))
+
+(def pmd                 (prefixer "http://publishmydata.com/def/dataset#"))
+
+(def pmd:Dataset         (pmd "Dataset"))
+
+(def pmd:contactEmail    (pmd "contactEmail"))
+(def pmd:graph           (pmd "graph"))
 
 
-(def pmd:contactEmail (pmd "contactEmail"))
-(def pmd:graph (pmd "graph"))
+(def folder              (prefixer "http://publishmydata.com/def/ontology/folder/"))
 
-(def pmd:Dataset (pmd "Dataset"))
+(def folder:Folder       (folder "Folder"))
+
+(def folder:hasTree      (folder "hasTree"))
+(def folder:defaultTree  (folder "defaultTree"))
+(def folder:parentFolder (folder "parentFolder"))
