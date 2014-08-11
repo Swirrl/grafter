245bf864509e2e725baaa3a6f990e08681e832e1
Robin Gower <robin.git@infonomics.ltd.uk>
2014-08-11 18:52:27 +0100

    dcterms:creator and org:Organization

--- src/grafter/rdf/ontologies/dcterms.clj
+++ src/grafter/rdf/ontologies/dcterms.clj
@@ -12,4 +12,5 @@
 (def dcterms:issued (dcterms "issued"))
 (def dcterms:license (dcterms "license"))
 (def dcterms:publisher (dcterms "publisher"))
+(def dcterms:creator (dcterms "creator"))
 (def dcterms:references (dcterms "references"))
---
+++ src/grafter/rdf/ontologies/org.clj
@@ -0,0 +1,6 @@
+(ns grafter.rdf.ontologies.org
+  (:use [grafter.rdf.ontologies.util]))
+
+(def org (prefixer "http://www.w3.org/ns/org#"))
+
+(def org:Organization (org "Organization"))
