36b291b327aa324b3010d5f930470fbb110a333b
Rick Moynihan <rick.m@swirrl.com>
2014-08-19 17:41:46 +0100

    Added doc strings to sequences functions.

--- src/grafter/sequences.clj
+++ src/grafter/sequences.clj
@@ -2,15 +2,27 @@
   "A library of useful lazy sequences.")
 
 (defn integers-from
-  ([n]
-     (iterate inc n)))
+  "Returns an infinite sequence of integers counting from the supplied
+  starting number.
 
-(defn column-names-seq [alphabet]
-  "Given an alphabet string generate a lazy sequences of column
+  Supports the use of an optional increment amount to allow increments
+  of arbitrary or negative amounts.
+
+  If no arguments are supplied an infinite sequence of all positive
+  integers from 1 is returned."
+  ([]
+     (iterate inc 1))
+  ([from]
+     (iterate inc from))
+  ([from inc-by]
+     (iterate #(+ % inc-by) from)))
+
+(defn column-names-seq "Given an alphabet string generate a lazy sequences of column
   names e.g.
 
   (column-names-seq \"ABCDEFGHIJKLMNOPQRSTUVWXYZ\") ;; => (\"A\" \"B\" \"C\" ... \"AA\" \"AB\")
 "
+  [alphabet]
   (->> (map str alphabet)
        (iterate (fn [chars]
                   (for [x chars
