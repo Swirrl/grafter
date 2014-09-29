eb4c420ee167fe4c16bb938d439bd80b3ea8afa4
Rick Moynihan <rick.m@swirrl.com>
2014-09-29 15:52:56 +0100

    Add some foaf properties to the foaf namespace for our example.

--- src/grafter/rdf/ontologies/foaf.clj
+++ src/grafter/rdf/ontologies/foaf.clj
@@ -2,5 +2,12 @@
   (:use [grafter.rdf.ontologies.util]))
 
 (def foaf (prefixer "http://xmlns.com/foaf/0.1/"))
-
+(def foaf:Person (foaf "Person"))
+(def foaf:age (foaf "age"))
 (def foaf:depiction (foaf "depiction"))
+(def foaf:gender (foaf "gender"))
+(def foaf:homepage (foaf "homepage"))
+(def foaf:interest (foaf "interest"))
+(def foaf:knows (foaf "knows"))
+(def foaf:name (foaf "name"))
+(def foaf:nick (foaf "nick"))
