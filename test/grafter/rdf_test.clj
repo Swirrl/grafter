218f11fe41e4c62ca9ecb0af8c919f4b1ba67cc7
Rick Moynihan <rick.m@swirrl.com>
2015-01-20 12:52:50 +0000

    Move test for graph-fn to tabular-test ns

--- test/grafter/rdf_test.clj
+++ test/grafter/rdf_test.clj
@@ -12,48 +12,6 @@
 
 (def second-quad (->Quad "http://a2" "http://b2" "http://c2" "http://graph2"))
 
-(deftest graph-fn-test
-  (testing "graph-fn"
-    (testing "destructuring"
-      (are [name column-names binding-form body]
-        (testing name
-          (let [ds (make-dataset test-data
-                                 column-names)
-                f (graph-fn [binding-form]
-                            body)]
-
-            (is (= first-quad
-                   (first (f ds))))))
-
-        "by :keys"
-        [:a :b :c :d] {:keys [a b c d]}
-        (graph d
-               [a [b c]])
-
-        "by :strs"
-        ["a" "b" "c" "d"] {:strs [a b c d]}
-        (graph d
-               [a [b c]])
-
-        "by map"
-        ["a" :b "c" :d] {a "a" b :b c "c" graf :d}
-        (graph graf
-               [a [b c]])
-
-        "by position (vector)"
-        [:a :b :c :d] [one two three graf]
-        (graph graf
-               [one [two three]])))
-
-    (testing "concatenates sequences returned by each form in the body"
-      (let [ds (make-dataset test-data)
-                f (graph-fn [[one two three graf]]
-                            (graph graf
-                                   [one [two three]]))]
-
-        (is (= [first-quad second-quad]
-               (f ds)))))))
-
 (deftest quads-and-triples-test
   (testing "Triples"
     (testing "support positional destructuring"
--- test/grafter/tabular_test.clj
+++ test/grafter/tabular_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [grafter.sequences :as seqs]
             [grafter.tabular :refer :all]
+            [grafter.rdf.protocols :refer [->Quad]]
+            [grafter.rdf.templater :refer [graph triplify]]
             [grafter.tabular.csv]
             [grafter.tabular.excel]
             [incanter.core :as inc]
@@ -509,3 +511,50 @@
         (with-tempfile xls-file
           (write-dataset xls-file sample-dataset :format :xls)
           (is (= sample-dataset (make-dataset (read-dataset xls-file :format :xls) move-first-row-to-header))))))))
+
+(deftest graph-fn-test
+  (let [test-data [["http://a1" "http://b1" "http://c1" "http://graph1"]
+                   ["http://a2" "http://b2" "http://c2" "http://graph2"]]
+        first-quad (->Quad "http://a1" "http://b1" "http://c1" "http://graph1")
+        second-quad (->Quad "http://a2" "http://b2" "http://c2" "http://graph2")]
+
+    (testing "graph-fn"
+      (testing "destructuring"
+        (are [name column-names binding-form body]
+          (testing name
+            (let [ds (make-dataset test-data
+                                   column-names)
+                  f (graph-fn [binding-form]
+                              body)]
+
+              (is (= first-quad
+                     (first (f ds))))))
+
+          "by :keys"
+          [:a :b :c :d] {:keys [a b c d]}
+          (graph d
+                 [a [b c]])
+
+          "by :strs"
+          ["a" "b" "c" "d"] {:strs [a b c d]}
+          (graph d
+                 [a [b c]])
+
+          "by map"
+          ["a" :b "c" :d] {a "a" b :b c "c" graf :d}
+          (graph graf
+                 [a [b c]])
+
+          "by position (vector)"
+          [:a :b :c :d] [one two three graf]
+          (graph graf
+                 [one [two three]])))
+
+      (testing "concatenates sequences returned by each form in the body"
+        (let [ds (make-dataset test-data)
+              f (graph-fn [[one two three graf]]
+                          (graph graf
+                                 [one [two three]]))]
+
+          (is (= [first-quad second-quad]
+                 (f ds))))))))
