d16fc306afab1d047af39ba0238ad18d0019d101
lkitching <lee@swirrl.com>
2015-01-26 17:18:48 +0000

    Create generalised lazy version of melt.
    Create a generalised version of melt and implement melt and
    melt-column-groups using it. The generalised melt takes a function
    to partition the non-fixed columns and generates n rows in the output
    for each row in the input, where n is the number of generated column
    partitions. Rows in the output are generated by a function which
    returns a map for the variable part of the row given the input row and
    the generated column partition.
    
    Melt is then implemented by returning n single-column partitions
    and mapping each of these to a two-column fragment in the output
    with 'value' and 'variable' columns.
    
    Melt column groups is implemented by partitioning the columns into
    n m-length partitions where m is the number of specified output
    columns. The columns in each partition are then renamed to match
    those specified.
    
    Add tests for melt and melt-column-groups functions.

--- test/grafter/tabular_test.clj
+++ test/grafter/tabular_test.clj
@@ -664,3 +664,47 @@
           (is (= {"a" "http://one/" "b" "http://two/" "c" "http://three/"}
                  (:grafter.tabular/row quad-meta))
               "Adds the row that yielded each Quad as metadata"))))))
+
+(defn datasets-equal?
+  "Compares two datasets for equality ignoring the order of rows. Two datasets are considered equal if they have the same column
+   names in the same order, and have the same rows in any order."
+  [ds1 ds2]
+  (and (= (column-names ds1) (column-names ds2))
+       (= (set (:rows ds1)) (set (:rows ds2)))))
+
+(deftest melt-test
+  (testing "Melts dataset"
+    (let [initial (make-dataset [[:sales 100 200 300]
+                                 [:costs 500 400 300]]
+                                [:measure :2012 :2013 :2014])
+
+          melted (melt initial :measure)
+          expected (make-dataset [[:sales :2012 100]
+                                  [:sales :2013 200]
+                                  [:sales :2014 300]
+                                  [:costs :2012 500]
+                                  [:costs :2013 400]
+                                  [:costs :2014 300]]
+                                 [:measure :variable :value])]
+      (is (datasets-equal? melted expected)))))
+
+(deftest melt-column-groups-test
+  (testing "Melt groups of columns"
+    (let [denormalised (make-dataset [[1 2 3 4 5 6 7 8]
+                                      [9 10 11 12 13 14 15 16]]
+                                     [:a :b, :c :d, :e :f, :g :h])
+
+          normalised (melt-column-groups denormalised [:a :b] [:x :y])
+          expected (make-dataset [[1 2 3 4]
+                                  [9 10 11 12]
+                                  [1 2 5 6]
+                                  [9 10 13 14]
+                                  [1 2 7 8]
+                                  [9 10 15 16]]
+                                 [:a :b :x :y])]
+      (is (datasets-equal? normalised expected))))
+
+  (testing "Throws on invalid column group size"
+    (let [initial (make-dataset [[1 2 3 4]]
+                                [:fixed :a :b :c])]
+      (is (thrown? IllegalArgumentException (melt-column-groups initial [:fixed] [:c1 :c2]))))))
