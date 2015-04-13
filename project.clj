85dd1014234afbd9d76144e18e86f96900ba1381
Rick Moynihan <rick.m@swirrl.com>
2015-04-13 12:49:28 +0100

    version bump to 0.5.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grafter/grafter "0.4.1-SNAPSHOT"
+(defproject grafter/grafter "0.5.0-SNAPSHOT"
   :description "Tools for the hard graft of data processing"
   :url "http://grafter.org/"
   :license {:name "Eclipse Public License - v1.0"
