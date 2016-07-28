4f6b184ff336d7c1e2eb1b05dbc0830352ad083d
Scott Lowe <scottwithtwotees@gmail.com>
2016-07-28 11:38:27 +0100

    Add SPARQL LIMIT clause pre-processing step for dynamic LIMIT query bindings

--- src/rdf-repository/grafter/rdf/sparql.clj
+++ src/rdf-repository/grafter/rdf/sparql.clj
@@ -3,27 +3,45 @@
             [grafter.rdf.repository :refer [repo sparql-repo ->connection]]
             [grafter.rdf.repository :as repo]
             [grafter.rdf.io :refer [->sesame-rdf-type]]
-            [clojure.java.io :refer [resource]]))
+            [clojure.java.io :refer [resource]]
+            [clojure.string :as str]))
+
+(defn pre-process-limit-clauses
+  "look through a query string and replaces limit clauses with values supplied
+  as maps against matching SPARQL ?variable names or a limit integer"
+  [sparl-query limits]
+  (reduce (fn [memo [key val]]
+            (if-let [pattern (cond
+                               (integer? key)
+                               (str "(?i)LIMIT\\s+" key)
+
+                               (or (string? key) (keyword? key))
+                               (str "(?i)LIMIT\\s+\\?" (name key)))]
+              (str/replace memo
+                           (re-pattern pattern)
+                           (str "LIMIT " val))
+              memo))
+          sparl-query
+          limits))
 
 (defn query
   ([sparql-file repo]
    (query sparql-file repo {}))
   ([sparql-file repo bindings]
    (let [sparql-query (slurp (resource sparql-file))
-         preped-query (repo/prepare-query repo sparql-query)]
+         pre-processed-qry (pre-process-limit-clauses sparql-query
+                                                      (or (:limits bindings) []))
+         prepped-query (repo/prepare-query repo pre-processed-qry)]
      (reduce (fn [pq [unbound-var val]]
                (.setBinding pq (name unbound-var) (->sesame-rdf-type val))
-               pq) preped-query bindings)
+               pq)
+             prepped-query
+             (dissoc bindings :limits))
 
-     (repo/evaluate preped-query))))
+     (repo/evaluate prepped-query))))
 
 (comment
-
-
   (def r (repo "/Users/rick/repos/grafter/test/grafter/rdf/sparql-data.trig"))
 
   (query "grafter/rdf/select-spog.sparql" (->connection r) {"s" (java.net.URI. "http://example.org/data/a-triple")})
-
-
-
   )
--- test/grafter/rdf/sparql_test.clj
+++ test/grafter/rdf/sparql_test.clj
@@ -1,19 +1,26 @@
 (ns grafter.rdf.sparql-test
   (:require [grafter.rdf.sparql :refer :all]
             [clojure.test :refer :all]
+            [clojure.java.io :refer [resource]]
             [grafter.rdf.repository :refer [repo]])
-  (:import  java.net.URI))
+  (:import java.net.URI))
 
+(deftest pre-process-limit-clauses-test
+  (let [sparql-file (slurp (resource "./grafter/rdf/select-spog-unprocessed.sparql"))
+        processed-sparql-file (slurp (resource "./grafter/rdf/select-spog-pre-processed.sparql"))]
+    (is (= (pre-process-limit-clauses sparql-file
+                                      {:myLimitVar 55
+                                       7 39})
+           processed-sparql-file))))
 
 (deftest query-test
   (let [r (repo "./test/grafter/rdf/sparql-data.trig")
-        spog (partial query "./grafter/rdf/select-spog.sparql")
-
-        query-result (first (spog r {:s (URI. "http://example.org/data/a-triple")}))]
-
+        spog (partial query "./grafter/rdf/select-spog-unprocessed.sparql")
+        query-result (first (spog r {:s (URI. "http://example.org/data/a-triple")
+                                     :limits {"myLimitVar" 349
+                                              1 2}}))]
     (is (= query-result
            {:s (URI. "http://example.org/data/a-triple")
             :p (URI. "http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
             :o (URI. "http://example.org/data/Quad")
-            :g (URI. "http://example.org/graph/more-quads")})
-        )))
+            :g (URI. "http://example.org/graph/more-quads")}))))
