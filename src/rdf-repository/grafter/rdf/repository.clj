87bd0adab4ff17b00c8711b6ba595f5be6863be1
Rick Moynihan <rick.m@swirrl.com>
2016-07-28 18:27:39 +0100

    Wrap stacktrace properly to capture applicaton context.  Fix #77.

--- src/rdf-repository/grafter/rdf/repository.clj
+++ src/rdf-repository/grafter/rdf/repository.clj
@@ -387,7 +387,7 @@
                                                 (converter-f (.next results))
                                                 (catch Exception e
                                                   (.close results)
-                                                  (throw e)))]
+                                                  (throw (ex-info "Error reading results" {:prepared-query prepared-query} e))))]
                            (lazy-cat
                             [current-result]
                             (pull-query)))
