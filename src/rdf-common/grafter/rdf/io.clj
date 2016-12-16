e948d4ecb7f4155740f4322a84a61549460e7591
Rick Moynihan <rick.m@swirrl.com>
2016-12-16 09:36:51 +0000

    Fix serialising custom literals

--- src/rdf-common/grafter/rdf/io.clj
+++ src/rdf-common/grafter/rdf/io.clj
@@ -131,7 +131,7 @@
 
   RDFLiteral
   (->sesame-rdf-type [this]
-    (LiteralImpl. (pr/raw-value this) (URIImpl. (pr/datatype-uri this))))
+    (LiteralImpl. (pr/raw-value this) (URIImpl. (str (pr/datatype-uri this)))))
 
   (sesame-rdf-type->type [this]
     this)
