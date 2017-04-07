14c6d00f9e2fe14dfec99b9bdfc88c993e6fff9b
Rick Moynihan <rick.m@swirrl.com>
2017-04-07 12:27:59 +0100

    post coverage reports to coveralls

---
+++ travis/coveralls.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# POST job to coveralls.  NOTE that running lein cloverage under
+# travis automatically writes the appropriate details of the
+# repo/build etc into the coveralls.json.
+
+COVERALLS_URL='https://coveralls.io/api/v1/jobs'
+curl -F 'json_file=@target/cloverage/coveralls.json' "$COVERALLS_URL"
