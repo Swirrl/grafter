c5de20d507d38cf4d23e86ef049d2f92686004b4
Rick Moynihan <rick.m@swirrl.com>
2017-11-05 17:14:20 +0000

    Bump dep

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grafter/grafter "0.10.2-SNAPSHOT"
+(defproject grafter/grafter "0.11.0-alpha1-SNAPSHOT"
   :description "Tools for the hard graft of linked data processing"
   :url "http://grafter.org/"
   :license {:name "Eclipse Public License - v1.0"
