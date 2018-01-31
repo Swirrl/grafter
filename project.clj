1c993c26c4185b331c5c8a76858eca17ec0bc9b0
Rick Moynihan <rick.m@swirrl.com>
2018-01-31 16:56:42 +0000

    lock version for drafter/stasher

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grafter/grafter "0.11.0-alpha1-SNAPSHOT"
+(defproject grafter/grafter "0.11.0-drafter-rdf4j"
   :description "Tools for the hard graft of linked data processing"
   :url "http://grafter.org/"
   :license {:name "Eclipse Public License - v1.0"
