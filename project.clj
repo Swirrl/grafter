0c20177754826717c64fb04df47c72e04c74c403
Dan Midwood <dan@swirrl.com>
2018-07-11 11:28:00 +0100

    Version 0.11.0.2-drafter-rdf4j

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grafter/grafter "0.11.0.2-drafter-rdf4j-SNAPSHOT"
+(defproject grafter/grafter "0.11.0.2-drafter-rdf4j"
   :description "Tools for the hard graft of linked data processing"
   :url "http://grafter.org/"
   :license {:name "Eclipse Public License - v1.0"
