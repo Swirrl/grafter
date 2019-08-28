3b8e10263a327721fdfbc113cd6984e2f35df2a3
Rick Moynihan <rick.m@swirrl.com>
2019-08-28 15:47:49 +0100

    Version 2.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grafter/grafter "2.0.3-SNAPSHOT"
+(defproject grafter/grafter "2.0.3"
   :description "Tools for the hard graft of linked data processing"
   :url "http://grafter.org/"
   :license {:name "Eclipse Public License - v1.0"
